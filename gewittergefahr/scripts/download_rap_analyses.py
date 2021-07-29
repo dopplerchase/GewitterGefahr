b3e895449cd9eb30eff2394c9af094b2f80556bd
Randy J. Chase <randy.chase12@gmail.com>
2021-07-29 13:12:03 -0500

    bug fix

--- gewittergefahr/scripts/download_rap_analyses.py
+++ gewittergefahr/scripts/download_rap_analyses.py
@@ -53,7 +53,7 @@ def _download_rap_analyses(first_init_time_string, last_init_time_string,
     first_init_time_string  = first_init_time_string + '00'
     s_dtime = datetime.datetime.strptime(first_init_time_string,'%Y%m%d%H')
     e_dtime = s_dtime + datetime.timedelta(days=1)
-    last_init_time_unix_sec = e_dtime.strftime('%Y%m%d%H')
+    last_init_time_string = e_dtime.strftime('%Y%m%d%H')
 
     first_init_time_unix_sec = time_conversion.string_to_unix_sec(
         first_init_time_string, INPUT_TIME_FORMAT
